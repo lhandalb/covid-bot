a08d52e1d490fccf40f7376160238d5d4e516a5d
lhandalb <lhandalb@github.com>
2020-06-03 20:24:30 -0400

    Minor fix

--- app.py
+++ app.py
@@ -9,11 +9,10 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def bot():
     incoming_msg = request.values.get('Body', '').lower()
-    #print(incoming_msg)
     resp = MessagingResponse()
     msg = resp.message()
     text = ""
-    menu_txt = '\n\nSend a *country name*, or *global*, for the latest report\nSend *symptoms* to learn how the virus spreads\nSend *prevent* to see how to prevent the disease'
+    menu_txt = '\n\nSend a *country name*, or *global*, for the latest report\nSend *symptoms* to see the disease\'s symptoms\nSend *prevent* to learn how to prevent the disease'
 
     if 'hi' == incoming_msg or 'hey' == incoming_msg or 'hello' == incoming_msg or 'hola' == incoming_msg:
         text = 'Hey! \nThis is Ms Rona with the latest COVID-19 updates. How can I help you?'
@@ -25,12 +24,10 @@ def bot():
     elif 'symptoms' in incoming_msg:
         text = f'COVID-19 sympotoms:\n\nCommonn symptoms:\n-fever\n-dry cough\n-tiredness\n\nLess common symptoms:\n-aches and pains\n-sore throat\n-diarrhoea\n-conjunctivitis\n-headache\n-loss of taste or smell\n-a rash on skin, or discolouration of fingers or toes\n\nSerious symptoms:\n-difficulty breathing or shortness of breath\n-chest pain or pressure\n-loss of speech or movement\n\nSeek immediate medical attention if you have serious symptoms. Always call before visiting your doctor or health facility.\nPeople with mild symptoms who are otherwise healthy should manage their symptoms at home.\nOn average it takes 5–6 days from when someone is infected with the virus for symptoms to show, however it can take up to 14 days.\n\nLearn more on https://www.who.int/emergencies/diseases/novel-coronavirus-2019/question-and-answers-hub/q-a-detail/q-a-coronaviruses#:~:text=symptoms'
         msg.body(text + menu_txt)
-        # msg.media('https://user-images.githubusercontent.com/34777376/77290801-f2421280-6d02-11ea-8b08-fdb516af3d5a.jpeg')
 
     elif 'prevent' in incoming_msg or 'spread' in incoming_msg:
         text = f'_COVID-19 spread can be prevented through the following means_\n\n*Stay* home as much as you can\n*Keep* a safe distance\n*Wash* hands often\n*Cover* your nose and mouth\n\nlearn more on https://www.who.int/emergencies/diseases/novel-coronavirus-2019/advice-for-public'
         msg.body(text + menu_txt)
-        # msg.media('https://user-images.githubusercontent.com/34777376/77290864-1c93d000-6d03-11ea-96fe-18298535d125.jpeg')
 
     elif 'world' in incoming_msg or 'planet' in incoming_msg or 'global' in incoming_msg or 'all' == incoming_msg:
         # send global report
